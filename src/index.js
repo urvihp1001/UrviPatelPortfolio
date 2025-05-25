ba11642288f39afcf9b532cf6fc43b0b4fa871ce
urvihp1001 <urvi.email@gmail.com>
2025-05-25 16:59:43 +0530

    changed the styling

--- src/index.js
+++ src/index.js
@@ -3,14 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
-
+//BrowserRouter is used to enable routing in the application
+// It allows us to define routes and navigate between different components
+// If you want to enable strict mode, you can wrap your app in <React.StrictMode>
+// Strict mode helps identify potential problems in an application
+// It activates additional checks and warnings for its descendants
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
